1dd4262209ef4a122a92f6420e1197ce03512c97
cervols <paprikamail@gmail.com>
2023-11-08 14:22:14 +0100

    [+] Add simple ApiKey model to store access_tokens

---
+++ app/models/api_key.rb
@@ -0,0 +1,11 @@
+class ApiKey < ApplicationRecord
+  validates :access_token, presence: true, uniqueness: true
+
+  before_validation :generate_access_token
+
+  private
+
+    def generate_access_token
+      self.access_token = Digest::MD5.hexdigest(SecureRandom.hex)
+    end
+end
---
+++ spec/models/api_key_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe ApiKey, type: :model do
+  it 'has valid factory' do
+    expect(build(:api_key)).to be_valid
+  end
+end
